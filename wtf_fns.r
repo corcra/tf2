d4f9833c0bf7f37e097adcc30f717530a8a9ff8c
Stephanie Hyland <steph.hyland@gmail.com>
2013-12-08 17:11:16 -0500

    extracted the functions i am confident work correctly

--- wtf_fns.r
+++ wtf_fns.r
@@ -1,36 +1,8 @@
 # This code contains functions for wtf.r
-cat('Loaded functions!\n')
-
-# note! right now this only works for one factor... but could be extended
-get_motif<-function(factor){
-    pfm <- read.table('CTCF.NGCGCCMYCTAGYGGTN.pwm',skip=2)
-# including some pseudo-counts... maybe unnecessary?
-        pseudo_pfm <- matrix(rep(1,prod(dim(pfm))),dim(pfm))
-        adj_pfm <- pfm+pseudo_pfm
-# note taking a transpose!
-        pwm <- t(adj_pfm/rowSums(adj_pfm))
-        return(pwm)
-}
+cat('Loaded potentially-dodgy functions!\n')
 
 get_features<-function(DNase_data){
 # what are the features, eh?
-}
-
-is_bound<-function(probs){
-    success <- runif(length(probs))<probs
-# a single success is sufficient for binding!
-        bound_yn <- ifelse((sum(success)==0),0,1)
-        return(bound_yn)
-}
-
-include_pwm<-function(hmm,pwm,N_STATES){
-# assume background and generic states have equal probs
-    set.emission.params.qhmm(hmm, c(1,N_STATES), rep(0.25,4), slot=1, fixed=rep(T,4))
-# the pwm is just a matrix, whose columns refer to positions and rows refer to ACGT
-        for (state in 2:(N_STATES-1)){
-            set.emission.params.qhmm(hmm, state, pwm[,(state-1)], slot=1, fixed=rep(T,4))
-        }
-    return(hmm)
 }
 
 build_hmm<-function(N_STATES,N_FEATURES,pwm){
@@ -38,26 +10,26 @@ build_hmm<-function(N_STATES,N_FEATURES,pwm){
     data_shape <- list(rep(1,N_FEATURES+1),NULL)
 
 # construct the valid transitions! this is a little tricky because the number of states depends on the transcription factor
-        valid_transitions <- matrix(rep(0,N_STATES*N_STATES),nrow=N_STATES,ncol=N_STATES,byrow=T)
-        valid_transitions[1,] <- c(1,2,rep(0,(N_STATES-3)),3)
-        valid_transitions[2:(N_STATES-2),3:(N_STATES-1)] <- diag(N_STATES-3)
-        valid_transitions[(N_STATES-1),] <- c(1,rep(0,(N_STATES-1)))
-        valid_transitions[N_STATES,] <- c(1, rep(0,(N_STATES-2)), 2)
+    valid_transitions <- matrix(rep(0,N_STATES*N_STATES),nrow=N_STATES,ncol=N_STATES,byrow=T)
+    valid_transitions[1,] <- c(1,2,rep(0,(N_STATES-3)),3)
+    valid_transitions[2:(N_STATES-2),3:(N_STATES-1)] <- diag(N_STATES-3)
+    valid_transitions[(N_STATES-1),] <- c(1,rep(0,(N_STATES-1)))
+    valid_transitions[N_STATES,] <- c(1, rep(0,(N_STATES-2)), 2)
 # everything is discrete here!
-        transition_functions <- rep("discrete",N_STATES)
+    transition_functions <- rep("discrete",N_STATES)
 # noo, a for loop!
-        emission_functions <- vector(mode="list",length=N_STATES)
-        for (state in 1:N_STATES){
-            emission_functions[[state]]<-rep("discrete",(N_FEATURES+1))
-        }
+    emission_functions <- vector(mode="list",length=N_STATES)
+    for (state in 1:N_STATES){
+        emission_functions[[state]]<-rep("discrete",(N_FEATURES+1))
+    }
 # create the HMM
     hmm <- new.qhmm(data_shape,valid_transitions,transition_functions,emission_functions,support.missing=TRUE)
 
 # set the fixed parameters
-        set.transition.params.qhmm(hmm,2:(N_STATES-1),1,fixed=T)
+    set.transition.params.qhmm(hmm,2:(N_STATES-1),1,fixed=T)
 # include the pwm info!
-        hmm <- include_pwm(hmm,pwm,N_STATES)
-        return(hmm)
+    hmm <- include_pwm(hmm,pwm,N_STATES)
+    return(hmm)
 }
 
 initialise_hmm<-function(hmm,N_STATES,N_FEATURES,transition_params,emission_params){
@@ -79,44 +51,11 @@ initialise_hmm<-function(hmm,N_STATES,N_FEATURES,transition_params,emission_para
             theta <- emission_params[state,feature]
             if(theta==0|theta==1){
                 print("we're about to get an emission probability of zero...")
-                browser()
                 }
             set.emission.params.qhmm(hmm, state, c(1-theta,theta),slot=(feature+1))
             }
         }
     return(hmm)
-}
-
-
-# coincidence matrix is one row per transcription factor, first column is 'F's CDF given no phi', second is 'F's CDF with phi' approximately speaking...
-get_interactions<-function(factor,binding_status){
-# binding of factor of interest
-    reference<-binding_status[,factor]
-# all the others
-        rest_mat<-binding_status[,!colnames(binding_status)==factor]
-# if they're both bound, we see 2!
-        comp_mat<-rest_mat+reference
-# if other isn't bound, but ref is, we see -1!
-        cont_mat<-rest_mat-reference
-# how often do we see both, given we saw 'other'?
-        if_yes<-colSums(comp_mat==2)/colSums(rest_mat==1)
-# how often do we see ref and not other, given no other?
-        if_no<-colSums(cont_mat==-1)/colSums(rest_mat==0)
-# turn this into a matrix, and pass it back
-        coincidence_mat<-matrix(c(if_no,if_yes),nrow=(N_FACTORS-1),ncol=2)
-# remove the NaNs... they should be zeroes anyway!
-        coincidence_mat[is.nan(coincidence_mat)]<-0
-        return(coincidence_mat)
-}
-
-get_C_int<-function(peak,peak_length,factor,factor_size,binding_status,coincidence){
-# get the peak-specific bound status, excluding current factor
-    peak_bound_status<-as.numeric(binding_status[peak,!colnames(binding_status)==factor])
-# translate this into a matrix of indices to query the coincidence matrix - the query will produce a vector of the relevant elements of the coincidence matrix depending on whether or not the factor is actully bound in this peak!
-    relevant_indices<-matrix(c(seq(N_FACTORS-1),peak_bound_status+1),(N_FACTORS-1),2)
-# some normalisation, sum over aforementioned vector...
-    C_int <- (1.0/((peak_length-factor_size)*(N_FACTORS-1)))*sum(coincidence[relevant_indices])
-    return(C_int)
 }
 
 get_emission_prob<-function(hmm,peak_data,peak_length,state,N_FEATURES){
@@ -126,7 +65,7 @@ get_emission_prob<-function(hmm,peak_data,peak_length,state,N_FEATURES){
         # this line might look a bit weird, but the value in the peak_data is actually the label found in the hmm
         # also note: we're actually only interested in the probs from 2... end... see the mathematical form!
         probs<-probs*emission_probs[peak_data[slot_num,2:peak_length]]
-        }
+    }
     return(probs)
 }
 
@@ -197,13 +136,5 @@ get_theta_and_xi<-function(hmm,peak_data,peak_length,N_STATES,N_FEATURES){
     xi_GB <- sum(exp(alpha_prime[N_STATES,1:(peak_length-1)]+beta_prime[1,2:peak_length]+log(emissions_B)+log(a_GB)-ll))
     xi_GG <- sum(exp(alpha_prime[N_STATES,1:(peak_length-1)]+beta_prime[N_STATES,2:peak_length]+log(emissions_G)+log(a_GG)-ll))
 
-    if(!sum(is.finite(c(xi_BB,xi_BG,xi_GB,xi_GG)))==4){
-        browser()
-        }
-
     return(list("theta_numer"=theta_numer,"theta_denom"=theta_denom,"xi_BB"=xi_BB,"xi_BG"=xi_BG,"xi_BF"=xi_BF,"xi_GB"=xi_GB,"xi_GG"=xi_GG,"ll"=ll))
-}
-
-visualise_binding<-function(binding_status){
-    print(binding_status)
 }
