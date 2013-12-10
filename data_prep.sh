72304c3fdb4376a8155061634ab4c56585149065
Stephanie Hyland <steph.hyland@gmail.com>
2013-12-09 22:17:06 -0500

    updating this to be 'general' - also this ssh connection is way laggy so i want to pull this to my local computer

--- data_prep.sh
+++ data_prep.sh
@@ -1,19 +1,26 @@
-    # get the peaks!
-zcat k562_dnase/wgEncodeOpenChromDnaseK562PkV2.narrowPeak.gz | awk '{ print $1":"$2"-"$3+1 }' > k562_dnase_peaks
-    # get the sequence!
-twoBitToFa /gbdb/hg19/hg19.2bit peak_seq.fa -seqList=k562_dnase_peaks -noMask
-gzip peak_seq.fa
-    # format the sequence!
-python format_fasta.py peak_seq.fa.gz
-    # overlap with called peaks to get peak VALUES (in bedgraph format)!
-zcat ~/k562_dnase/wgEncodeOpenChromDnaseK562PkV2.narrowPeak.gz | bedmap --range 100 --echo --skip-unmapped wgEncodeOpenChromDnaseK562SigV2.bedGraph - > signal_in_peaks.bed
-gzip signal_in_peaks.bed
-    # process this into R-ready format! warning: this takes ages. second warning: file locations... check them!
-python format_bedgraph.py signal_in_peaks.bed.gz
-gzip processed_signal_in_peaks.bed
+DNASE_PEAKS=wgEncodeOpenChromDnaseK562PkV2.narrowPeak.gz
+DNASE_SIGNAL=wgEncodeOpenChromDnaseK562SigV2.bedGraph
+SIGNAL_IN_PEAKS=signal_in_peaks.bed
+PEAK_LIST=k562_dnase_peaks
+PEAK_SEQ=peak_seq.fa
 
-    # SPECIFIC TO MY DATA: the above bedgraph: signal_in_peaks appears to be a subset of the sequence data! ... except for a single region on the X chromosome? (not sure what's going on here!)
-    # THEREFORE: we shall trim down the sequence data so that it only contains those lines also in the signal data... (at this point, individual lines refer to peaks)... format_bedgraph produces a list of recorded peaks, so we use this
+
+# get the peaks!
+zcat $DNASE_PEAKS | awk '{ print $1":"$2"-"$3+1 }' > $PEAK_LIST
+# get the sequence!
+twoBitToFa /gbdb/hg19/hg19.2bit $PEAK_SEQ -seqList=$PEAK_LIST -noMask
+gzip $PEAK_SEQ
+# format the sequence! ... this creates a file called $PEAK_SEQ.gz.oneperline
+python format_fasta.py $PEAK_SEQ.gz
+# overlap with called peaks to get peak VALUES (in bedgraph format)!
+zcat $DNASE_PEAKS | bedmap --range 100 --echo --skip-unmapped  $DNASE_SIGNAL - > $SIGNAL_IN_PEAKS
+gzip $SIGNAL_IN_PEAKS
+# process this into R-ready format! warning: this takes ages. second warning: file locations... check them! ... this creates a file called processed.$SIGNAL_IN_PEAKS
+python format_bedgraph.py $SIGNAL_IN_PEAKS.gz
+gzip processed.$SIGNAL_IN_PEAKS.gz
+
+# SPECIFIC TO MY DATA: the above bedgraph: signal_in_peaks appears to be a subset of the sequence data! ... except for a single region on the X chromosome? (not sure what's going on here!)
+# THEREFORE: we shall trim down the sequence data so that it only contains those lines also in the signal data... (at this point, individual lines refer to peaks)... format_bedgraph produces a list of recorded peaks, so we use this
 bedmap --echo --skip-unmapped peak_seq.fa.gz.oneperline k562_dnase_peaks_totals > sequence_by_peaks.bed
 gzip sequence_by_peaks.bed
     # clean up
@@ -37,3 +44,8 @@ rm peak_seq.fa.gz.oneperline
 
     # doing the above ... this will produce merged_files.gz, which can then be used as input to the algorithm!
 python merge_files.py peak_seq_fa.gz.oneperline.gz feature1.gz feature2.gz ... featureN.gz
+
+
+
+# -- Now for ChIP-seq! -- #
+# Need to look for ChIP-seq peaks in the DNase peaks!
