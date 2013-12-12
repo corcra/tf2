2c8a76bfb91ba071eb0cde85cfc418be9e642bc2
corcra <steph.hyland@gmail.com>
2013-12-12 14:10:23 -0500

    final version???

--- data_prep.sh
+++ data_prep.sh
@@ -27,57 +27,37 @@ bedmap --echo --skip-unmapped signal_with_flanks.bed $DNASE_PEAKS  > dnase_signa
 gzip dnase_signal_final.bed
 # clean up!
 echo "Cleaning up!"
+rm -v k562_peak_list.exp
+rm -v signal_near_peaks.bed.gz
+rm -v signal_near_peaks.bed.exp.gz
 rm -v prescored.bed
 rm -v scored_in_peaks.bed
 rm -v signal_with_flanks.bed
-rm -v signal_near_peaks.bed.exp.gz
-rm -v signal_near_peaks.bed.gz
+
 #
 exit
 # get the features!
-echo "Extracting featuers with R!"
+echo "Extracting features with R!"
 R --file=wtf_features.r --args dnase_signal_final.gz
 
 # get the sequence!
+echo "Getting sequence!"
 awk '{ print $1":"$2"-"$3 }' $DNASE_PEAKS > peaklist_for_seq
 twoBitToFa /gbdb/hg19/hg19.2bit peakseq.fa -seqList=peaklist_for_seq -noMask
 gzip peakseq.fa
-# format the sequence! ... this creates a file called $PEAK_SEQ.gz.oneperline and also $PEAK_LIST.totals
+# format the sequence! ... this creates a file called peakseq.fa.gz.oneperline
 python format_fasta.py peakseq.fa.gz
-
-# SPECIFIC TO MY DATA: the above bedgraph: signal_in_peaks appears to be a subset of the sequence data! ... except for a single region on the X chromosome? (not sure what's going on here!)
-# THEREFORE: we shall trim down the sequence data so that it only contains those lines also in the signal data... (at this point, individual lines refer to peaks)... format_bedgraph produces a list of recorded peaks, so we use this
-bedmap --echo --skip-unmapped $PEAK_SEQ.gz.oneperline $DNASE_PEAKS > seq.bed
+# overlap the sequence with the peaks...
+echo "Overlapping sequence!"
+bedmap --echo --skip-unmapped peakseq.fa.gz.oneperline $DNASE_PEAKS > seq.bed
 gzip seq.bed
 # clean up
 rm -v peakseq.fa.gz.oneperline
 
-# by now we should have
-# signal_in_peaks and sequence_by_peaks, both gzipped 'bed' files...
-# sequence_by_peaks is ONE LINE PER PEAK, ready to be merged
-# signal_in_peaks is ONE LINE PER LOCATION, requiring additional formatting
-# Also: k562_dnase_peaks_totals which is ONE LINE PER PEAK
-# after formatting signal_in_peaks (into separate files per feature - one line per peak) we need to overlap with sequence_by_peaks on each of the resultant feature files to make sure they're 'aligned', as it were
-# then the merging can occur!
-    
-# Re the formatting signal_in_peaks: this requires...
-# Read the output of format_bedgraph... this has one line per location, and a list of the DNase values around that location!
-# These need to be processed into features! (probably use R to do this)
-# For each feature:
-# Output a new file with one line PER PEAK: columns are feature values for the locations in the peak.
-# Use merge_files.py on the seq data (output from format_fasta.py) and ALL the feature files!
-# There are probably better ways to do this (there are definitely better ways to do this) but this can do for now...
-
-    # doing the above ... this will produce merged_files.gz, which can then be used as input to the algorithm!
-python merge_files.py peak_seq_fa.gz.oneperline.gz feature1.gz feature2.gz ... featureN.gz
-
-
-
-# --- working files --- #
-SIGNAL_IN_PEAKS=signal_in_peaks.bed
-PEAK_LIST=k562_dnase_peaks
-PEAK_SEQ=peak_seq.fa
-PEAK_SEQ_WITHSIGNAL=seq_in_peaks.bed
-CHIP_PEAKS=peakSeq.Haibk562Ctcf.bed.gz
-
-
+# merge! this produces processed_data.gz which can be fed into the program...
+echo "Merging sequence and DNase features!"
+python merge_files.py seq.bed.gz feature1.bed.gz feature2.bed.gz ... featureN.bed.gz
+# final clean up, i guess
+# rm -v feature1.bed.gz
+# ...
+# rm -v featureN.bed.gz
