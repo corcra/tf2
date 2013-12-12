b948bb264f65131475a8f03b527d2a39328d8a33
corcra <steph.hyland@gmail.com>
2013-12-12 12:02:16 -0500

    let's just not worry about the peak_end part of the identifier

--- merge_files.py
+++ merge_files.py
@@ -11,12 +11,12 @@ firstfile = gzip.open(sys.argv[1])
 otherfiles = [gzip.open(arg) for arg in sys.argv[2:]]
 
 for line in firstfile:
-    identifier = line.split()[0:3]
+    identifier = line.split()[0:2]
     length = len(line.split())
     outfile.write(line)
     for otherfile in otherfiles:
         otherline = otherfile.readline()
-        other_ident = otherline.split()[0:3]
+        other_ident = otherline.split()[0:2]
         other_length = len(otherline.split())
         if not other_ident == identifier:
             print 'ERROR! DIFFERING IDENTIFIERS?'
@@ -25,6 +25,8 @@ for line in firstfile:
         elif not other_length == length:
             print 'ERROR! DIFFERING LENGTHS?'
             print length, other_length
+            print line.split()
+            print otherline.split()
             sys.exit()
         else:
             outfile.write(otherline)
