a8f6f482650c71a9266079ef0e431cfb8bd60fbd
corcra <steph.hyland@gmail.com>
2013-12-12 14:50:48 -0500

    added some directory structure - trying to clean this up! at least even if it doesn't get results right now, it will be comprehensible...

---
+++ preprocess/data_prep.sh
@@ -0,0 +1,63 @@
+# --- these are the input files --- #
+DNASE_PEAKS=wgEncodeOpenChromDnaseK562PkV2.narrowPeak
+DNASE_SIGNAL=wgEncodeUwDgfK562Sig.bedGraph.gz
+WIN_SIZE=50
+
+echo "Getting peaks!"
+awk '{ print $1":"$2"-"$3+1 }' $DNASE_PEAKS> k562_peak_list
+
+# expand the peaks! ... this creates $PEAK_LIST.exp (by the way, this produces 'expanded' peaks, including the flanking regions, so they need to be re-restricted later!
+python expand_peak.py k562_peak_list $WIN_SIZE
+echo "Restricting DNAse signal to be in/near peaks!"
+zcat $DNASE_SIGNAL | bedmap --echo --skip-unmapped - k562_peak_list.exp > signal_near_peaks.bed
+gzip signal_near_peaks.bed
+# expand this bedgraph (one score per location...) ... this generates $SIGNAL_IN_PEAKS.exp.gz
+python expand_bedgraph.py signal_near_peaks.bed.gz
+echo "Assigning scores to locations in peaks!"
+zcat signal_near_peaks.bed.exp.gz | bedmap --exact --echo --echo-map-score --delim '\t' k562_peak_list.exp - > prescored.bed
+# fill in zeroes!
+awk '{ if (NF==3) print $1"\t"$2"\t"$3"\tNA\t"0; else print $1"\t"$2"\t"$3"\tNA\t"$4 }' prescored.bed > scored_in_peaks.bed
+# now get the flanking regions for each location in each peak (the sed stuff is for getting rid of semicolons) (overlapping with self!)
+echo "Getting flanking signals!"
+bedmap --range 25 --echo --echo-map-score scored_in_peaks.bed | sed 's/[;|\|]/\t/g' > signal_with_flanks.bed
+# now re-restrict this to the original peak regions
+echo "Re-restricting locations (with flank info) to be inside peaks!"
+bedmap --echo --skip-unmapped signal_with_flanks.bed $DNASE_PEAKS  > dnase_signal_final.bed
+# ready to go to R!
+gzip dnase_signal_final.bed
+# clean up!
+echo "Cleaning up!"
+rm -v k562_peak_list.exp
+rm -v signal_near_peaks.bed.gz
+rm -v signal_near_peaks.bed.exp.gz
+rm -v prescored.bed
+rm -v scored_in_peaks.bed
+rm -v signal_with_flanks.bed
+
+#
+exit
+# get the features!
+echo "Extracting features with R!"
+R --file=wtf_features.r --args dnase_signal_final.gz
+
+# get the sequence!
+echo "Getting sequence!"
+awk '{ print $1":"$2"-"$3 }' $DNASE_PEAKS > peaklist_for_seq
+twoBitToFa /gbdb/hg19/hg19.2bit peakseq.fa -seqList=peaklist_for_seq -noMask
+gzip peakseq.fa
+# format the sequence! ... this creates a file called peakseq.fa.gz.oneperline
+python format_fasta.py peakseq.fa.gz
+# overlap the sequence with the peaks...
+echo "Overlapping sequence!"
+bedmap --echo --skip-unmapped peakseq.fa.gz.oneperline $DNASE_PEAKS > seq.bed
+gzip seq.bed
+# clean up
+rm -v peakseq.fa.gz.oneperline
+
+# merge! this produces processed_data.gz which can be fed into the program...
+echo "Merging sequence and DNase features!"
+python merge_files.py seq.bed.gz feature1.bed.gz feature2.bed.gz ... featureN.bed.gz
+# final clean up, i guess
+# rm -v feature1.bed.gz
+# ...
+# rm -v featureN.bed.gz
